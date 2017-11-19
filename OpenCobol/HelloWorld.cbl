5d91473603d3b9dfca4a12284276299153dc2584
maficccc@gmail.com <maficccc@gmail.com>
2017-11-19 21:03:23 +0100

    Updated example Hello World

--- OpenCobol/HelloWorld.cbl
+++ OpenCobol/HelloWorld.cbl
@@ -1,26 +1,21 @@
-
+      ******************************************************************
+      * Author: Maxfx                                                  *
+      * Revision: 31/08/2017                                           *
+      * Example Hello World, first program in COBOL (OpenCOBOL)        *
+      ******************************************************************
        IDENTIFICATION DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-       PROGRAM-ID. HelloWorld.
+       PROGRAM-ID. HELLO-WORLD.
        ENVIRONMENT DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        CONFIGURATION SECTION.
-      *-----------------------
        INPUT-OUTPUT SECTION.
-      *-----------------------
        DATA DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        FILE SECTION.
-      *-----------------------
        WORKING-STORAGE SECTION.
-      *-----------------------
        PROCEDURE DIVISION.
-      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        MAIN-PROCEDURE.
-      **
+
       * The main procedure of the program
-      **
-            DISPLAY "Hello world"
+            DISPLAY "Hello world!"
             STOP RUN.
-      ** add other procedures here
-       END PROGRAM HelloWorld.
+
+       END PROGRAM HELLO-WORLD.
